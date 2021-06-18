8e80ed8ee391ee6e445bf1516ee2967b5fb66268
Cristian Oliveros <3308@holbertonschool.com>
2021-06-18 16:21:26 -0700

    6-print_numberz.c is added

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-for (n = 0 ; n < 10 ; n++)
-{
-putchar(n);
-}
-putchar('\n');
-return (0);
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
