4556c85bd71c62f0339f77409f8a042874899ccb
Cristian Oliveros <3308@holbertonschool.com>
2021-06-21 14:50:05 -0500

    0-holberton.c[fix]

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * main - Entry point
- *
+ * main -  prints Holberton
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Holberton\n");
-return(0);
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+
+  return (0);
 }
