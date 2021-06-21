4013f3a3de195898d94bce0f920cd78389ecd86a
Cristian Oliveros <3308@holbertonschool.com>
2021-06-21 13:46:09 -0500

    2-print_alphabet_x10.c[fix]

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
 
 void print_alphabet_x10(void)
 {
-  char ch, n;
-  for(n = 0; n <= 9; n++)
-    {
-      for(ch = 'a'; ch <= 'z'; ch++)
-        {
-          _putchar(ch);
-        }
-      _putchar('\n');
-    }
+char ch, n;
+for (n = 0; n <= 9; n++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
 }
