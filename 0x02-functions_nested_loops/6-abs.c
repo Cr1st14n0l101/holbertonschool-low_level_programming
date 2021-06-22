4a449676a5c9f02cf284d61e83cc1c05ee8e58eb
Cristian Oliveros <3308@holbertonschool.com>
2021-06-21 19:10:01 -0500

    6-abs.c[fix]

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
+/**
+ *  _abs - computes the absolute value of an integer
+ * @i: the variable
+ *  Return: Always 0 (Success)
+ */
 int _abs(int i)
 {
-  if(i >= 0)
-    {
-      return(i);
-    }
-  return(-i);
+if (i >= 0)
+{
+return (i);
+}
+return (-i);
 }
