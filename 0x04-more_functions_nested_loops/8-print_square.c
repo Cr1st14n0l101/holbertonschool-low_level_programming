d9fcfb7f9502ad82dc347ad22761f5589e4d01c3
Cristian Oliveros <3308@holbertonschool.com>
2021-06-24 18:53:03 -0500

    added 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ * size: variable
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m;
+
+		for (m = 1; m <= size; m++)
+		{
+			int o;
+
+			for (o = 1; o <= size; o++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
