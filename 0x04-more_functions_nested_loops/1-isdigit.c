1e6d788bec60ff3b49ec3a9c14b9d2d746a42848
Cristian Oliveros <3308@holbertonschool.com>
2021-06-24 10:48:31 -0500

    added 1-isdigit.c[fix]

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 #include <ctype.h>
+
+/**
+ * _isdigit - check if is a digit
+ * @c: the variable
+ * Return: Always 1 if is a digit
+ */
 int _isdigit(int c)
 {
-    if (isdigit(c))
-    {
-        return(1);
-    }
-    else
-    {
-        return(0);
-    }
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
