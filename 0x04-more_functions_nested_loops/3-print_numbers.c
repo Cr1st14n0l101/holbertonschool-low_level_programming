4800b11c2a14623aaefa621cd39cdf20b0057724
Cristian Oliveros <3308@holbertonschool.com>
2021-06-24 12:04:32 -0500

    3-print_numbers.c[fix]

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
+/**
+ * print_numbers - prints from 0 to 9 numbres
+ *
+ * Return: void
+ */
 void print_numbers(void)
 {
-    int a;
-    for (a = 0; a <= 9; a++)
-    _putchar(a + '0');
-    _putchar('\n');
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		_putchar(a + '0');
+	_putchar('\n');
 }
