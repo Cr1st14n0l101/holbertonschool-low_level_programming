a2e42cfbfaf4a7588ba097fd832ce143b140d0b6
Cristian Oliveros <3308@holbertonschool.com>
2021-06-24 15:16:00 -0500

    added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the variable
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m;
+
+		for (m = 1; m <= n; m++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
