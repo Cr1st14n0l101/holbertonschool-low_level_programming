7bc96ee41d7ca22d55ab23b1f8279520e58bc488
Cristian Oliveros <3308@holbertonschool.com>
2021-06-25 17:47:22 -0500

    added 101-natural.c[fix]

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (i < 1024)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            j = j + i;   
-        }
-        ++i;
-    }
-    printf("%d\n", j);
-    return 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j = j + i;
+		}
+		++i;
+	}
+	printf("%d\n", j);
+	return 0;
 }
