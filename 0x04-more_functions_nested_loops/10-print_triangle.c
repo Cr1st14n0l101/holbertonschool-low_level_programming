0b493da29fc91415a9bc3cf453376d2672803853
Cristian Oliveros <3308@holbertonschool.com>
2021-06-24 22:28:43 -0500

    added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: the variable
+ * Return: void
+ */
+void print_triangle(int size)
+{
+    if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m;
+
+		for (m = 1; m <= size; m++)
+		{
+			int o;
+
+			for (o = m; o < size; o++)
+            {
+                _putchar(' ');
+            }
+            for (o = 1; o <= m; o++)
+            {
+                _putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
