d73ea289fb09894e5f34c0d8cd0ae2d9c2b2249e
Cristian Oliveros <3308@holbertonschool.com>
2021-06-25 14:01:37 -0500

    added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,48 @@
 #include "holberton.h"
 /**
- * 
- * 
- * 
+ * print_times_table - prints 3, 5, 12 tables
+ * @n: the variable
+ * Return: void
  */
 void print_times_table(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            _putchar('0');
-            _putchar(',');
-            for (j = 1; j <= n; j++)
-            {
-                if (i * j < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar((i * j % 10) + '0');
-                }
-                else if (i * j >= 10 && i * j <= 99)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar((i * j / 10) + '0');
-                    _putchar((i * j % 10) + '0');
-                }
-                else if (i * j >= 100 && i * j <= 200)
-                {
-                    _putchar(' ');
-                    _putchar((i * j / 100) + '0');
-                    _putchar((i * j /10 % 10) + '0');
-                    _putchar((i * j % 10) + '0');
-                }
-                if (j < 12)
-                {
-                    _putchar(',');
-                }
-            }
-            _putchar('\n');
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			_putchar(',');
+			for (j = 1; j <= n; j++)
+			{
+				if (i * j < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((i * j % 10) + '0');
+				}
+				else if (i * j >= 10 && i * j <= 99)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((i * j / 10) + '0');
+					_putchar((i * j % 10) + '0');
+				}
+				else if (i * j >= 100 && i * j <= 200)
+				{
+					_putchar(' ');
+					_putchar((i * j / 100) + '0');
+					_putchar((i * j / 10 % 10) + '0');
+					_putchar((i * j % 10) + '0');
+				}
+				if (j < 12)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
