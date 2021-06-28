1670b0c4a3f3bd6768b031aed31a4e055b598c2a
Cristian Oliveros <3308@holbertonschool.com>
2021-06-28 15:18:41 -0500

    added 3-puts.c[fix]

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- * 
- * 
- * 
+ * _puts - prints a sentence
+ * @str: the string
+ * Return: void
  */
 void _puts(char *str)
 {
-    int l = 0;
+	int l = 0;
 
-    while(str[l] != '\0')
-    {
-        _putchar(*str);
-        str++;
-    }
-    _putchar('\n');
+	while(str[l] != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
