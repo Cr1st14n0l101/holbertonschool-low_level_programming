28b36a23fee1d9db2f12962c28d8261e2ae811d0
Cristian Oliveros <3308@holbertonschool.com>
2021-06-29 22:09:16 -0500

    added 9-strcpy.c[fix]

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	}
 	n = j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
