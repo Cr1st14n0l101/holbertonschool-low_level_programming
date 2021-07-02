d38f2bc641e988fecd36a57a6582d35d8148ea87
Cristian Oliveros <3308@holbertonschool.com>
2021-07-02 12:24:14 -0500

    3-strcmp.c[fix]

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,33 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int l1, l2, f;
-	int i = 0;
-	int j = 0;
+	int f;
 
-	while (s1[i] != '\0')
+	if (*s1 < *s2)
 	{
-		i++;
+		f = (*s1 - *s2);
 	}
-	l1 = i - 1;
-
-	while (s2[j] != '\0')
+	else if (*s1 > *s2)
 	{
-		j++;
-	}
-	l2 = j - 1;
-
-	if (l1 < l2)
-	{
-		f = (s1[l1] - s2[l2]);
-	}
-	else if (l1 > l2)
-	{
-		f = (s1[l1] - s2[l2]);
+		f = (*s1 - *s2);
 	}
 	else
 	{
-		f = (s1[l1] - s1[l1]);
+		f = (*s1 - *s1);
 	}
 	return (f);
 }
