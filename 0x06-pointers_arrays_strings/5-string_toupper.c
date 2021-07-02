a78a1e70d14a040e2c6b0d104c176900c3740cd6
Cristian Oliveros <3308@holbertonschool.com>
2021-07-02 13:08:44 -0500

    added 5-string_toupper.c[fix]

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,15 @@
  */
 char *string_toupper(char *s)
 {
-    int c = 0;
+	int c = 0;
 
-    while (s[c] != '\0') 
-    {
-      if (s[c] >= 'a' && s[c] <= 'z') {
-         s[c] = s[c] - 32;
-      }
-      c++;
-    }
-   return (s);
+	while (s[c] != '\0')
+	{
+		if (s[c] >= 'a' && s[c] <= 'z')
+		{
+			s[c] = s[c] - 32;
+		}
+		c++;
+	}
+	return (s);
 }
