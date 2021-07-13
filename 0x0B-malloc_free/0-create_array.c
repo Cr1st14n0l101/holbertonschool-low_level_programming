47a13660dc40ad6971351dd92ef1740b01e51846
Cristian Oliveros <3308@holbertonschool.com>
2021-07-13 15:04:23 -0500

    0-create_array.c[fixed]

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	str = malloc((size + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
