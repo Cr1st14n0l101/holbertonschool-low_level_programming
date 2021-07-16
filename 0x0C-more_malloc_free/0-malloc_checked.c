f485787e345041aa0acbdc815d1c3dd164fde6bc
Cristian Oliveros <3308@holbertonschool.com>
2021-07-16 13:46:27 -0500

    added 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the variable
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+	if (str == NULL)
+	{
+		exit (98);
+	}
+	return (str);
+}
