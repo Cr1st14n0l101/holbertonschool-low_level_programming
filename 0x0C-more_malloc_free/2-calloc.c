862a71034b789da7a9a97c508dae930c2a10bed2
Cristian Oliveros <3308@holbertonschool.com>
2021-07-17 10:19:18 -0500

    space correction in line 15

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	void *ptr;
 
-	if (nmemb == 0|| size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = calloc(nmemb, sizeof(size));
