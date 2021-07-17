a24b2f8cda01b9db4ef46afc815aefcf1049419a
Cristian Oliveros <3308@holbertonschool.com>
2021-07-17 11:27:54 -0500

    1-string_nconcat.c[test]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,42 +10,41 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
-	int l = 0;
-	char *str;
+    int i;
+    int len ;
+	unsigned int len2;
+    unsigned int j;
+    char *newstr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
+    if (s1 == NULL || s2 == NULL)
+    {
+        s1 = "";
+        s2 = "";
+    }
+    for (len = 0; s1[len] != '\0'; len++)
+    {
+    }
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
-		;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	if (n >= len2)
 	{
-		;
-	}
-	if (n >= j)
-	{
-		n = j;
-	}
-
-	str = malloc((i + n) * sizeof(s1[0]));
-	if (str == NULL)
-	{
-		return (NULL);
+		n = len2;
 	}
 
-	for (k = 0; k < i; k++)
-	{
-		str[k] = s1[k];
-	}
-	for (k = k; k < i + n; k++, l++)
-	{
-		str[k] = s2[l];
-	}
-	str[k] = '\0';
-	return (str);
+    newstr = malloc((len + n + 1));
+    if (newstr == NULL)
+    {
+        return (00);
+    }
+    for (i = 0; i < len; i++)
+    {
+        newstr[i] = s1[i];
+    }
+    for (j = 0; j < n; j++)
+    {
+        newstr[i + j] = s2[j];
+    }
+    newstr[i + j] = '\0';
+    return (newstr);
 }
