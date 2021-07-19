5ba473aa74898a4891276350235f18be2d22f1cf
Cristian Oliveros <3308@holbertonschool.com>
2021-07-19 14:59:52 -0500

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_MACRO
+#define _DOG_MACRO
+
+/**
+ * struct dog - data of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
