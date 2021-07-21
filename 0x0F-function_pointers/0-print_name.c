02b4435d28a7e2c4d8021d24f3f4b5894eab0707
Cristian Oliveros <3308@holbertonschool.com>
2021-07-21 12:09:05 -0500

    cut white spaces in 4 and 8 lines

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print the name
+ * @name: pointer to he name
+ * @f: pointer to the function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		(*f)(name);
+}
