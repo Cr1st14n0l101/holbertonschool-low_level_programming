6bff238fbb3a1baf9b59f23916425c2354f9bfe9
Cristian Oliveros <3308@holbertonschool.com>
2021-08-09 15:45:54 -0500

    changes in the conditional in line 24

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 	tmp = write(fd, text_content, i);
-	if (!tmp)
+	if (tmp != i)
 		return (-1);
 
 	close(fd);
