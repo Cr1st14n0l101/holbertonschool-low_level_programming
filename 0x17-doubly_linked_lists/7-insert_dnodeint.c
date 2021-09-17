7a6f39e509167cc24477a480eaaaab1f90be0024
cristian oliveros <3308@holbertonschool.com>
2021-09-17 13:01:07 -0500

    added conditional in line 33

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+ #include "lists.h"
 
 /**
  * insert_dnodeint_at_index - Function that inserts
@@ -12,29 +12,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
 	dlistint_t *tmp = *h;
-	dlistint_t *tmp2 = *h;
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/*verify if the idx is into the range of the list*/
-	while (tmp2)
-	{
-		count++;
-		tmp2 = tmp2->next;
-	}
-	if (idx > count)
-		return (NULL);
-
 	new_node->n = n;
 
 	if (idx == 0)
 	{
-		new_node->next = tmp;
+		new_node->next = *h;
 		new_node->prev = NULL;
-		tmp = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 
